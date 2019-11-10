31e454cf72c35a12306bec1791cb6735e3aece6f
cogito93 <56675339+cogito93@users.noreply.github.com>
2019-11-11 01:12:27 +0900

    Update manage.sh

--- manage.sh
+++ manage.sh
@@ -3,9 +3,9 @@
 read -p "Press (r/t) to run/terminate jobs respectively: " r_or_t
 if [[ $r_or_t == "r" ]]
 then
-read -p "Number of jobs you want to run simultaneously: " nb_of_jobs
-read -p "Job number you want to start with: " start_nb
-echo Run\(s\) $nb_of_jobs job\(s\), starting with job number $start_nb
+read -p "Number of batch jobs you want to run simultaneously: " nb_of_jobs
+read -p "Tower number you want to start with: " start_nb
+echo Run\(s\) $nb_of_jobs job\(s\), starting with tower number $start_nb
 nohup ./sub_multiple.sh $nb_of_jobs $start_nb 2> /dev/null &
 elif [[ $r_or_t == "t" ]]
 then
@@ -13,7 +13,7 @@ echo Terminating jobs ...
 pid=`ps -ef | grep 'sub_multiple.sh' | awk 'NR==1 {print $2}'`
 kill ${pid}
 rm nohup.out
-condor_rm mskim
+condor_rm {userID}
 else
 echo Answer again
 read -p "Press (r/t) to run/terminate jobs respectively: " r_or_t
